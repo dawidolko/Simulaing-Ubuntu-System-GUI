f34fab23262e256894e3adea88c6e21ea12d493f
Dawid Olko <dawid_olko@outlook.com>
2023-07-20 10:59:30 +0200

    Add files via upload

---
+++ projects/main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World in PHP</title>
+</head>
+<body>
+    <?php
+    // Wypisanie napisu "Hello World" na stronie
+    echo "Hello World in PHP!";
+    ?>
+</body>
+</html>
